02997e3ae5d2f28336f0ef99d66f18a48005a436
Maksiks <maksiksking@gmail.com>
2025-05-23 23:11:35 +0300

    init: init + feat: did some stuff already

---
+++ app.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+
+const things = ref([
+  {
+    id: 1,
+    name: 'fuckingfast',
+    link: 'https://fuckingfast.net/',
+    img: 'https://placehold.co/600x400/EEE/31343C'
+  },
+  {
+    id: 2,
+    name: 'wait',
+    link: 'https://fuckingfast.net/',
+    img: 'https://placehold.co/600x400/EEE/31343C'
+  }
+])
+
+</script>
+
+<template>
+  <main>
+    <h1>Maksiks' list of nifty stuffs</h1>
+    <div class="content-cont">
+      <div v-for="thing in things" :key="thing.id" class="thing">
+        <img :src="thing.img" :alt="thing.name" />
+      </div>
+    </div>
+  </main>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+html, body {
+  overflow-x: hidden;
+}
+</style>
+<style scoped>
+main {
+  & h1 {
+    color: yellow;
+  }
+
+
+}
+</style>
